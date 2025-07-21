e22b080a8dc44620ac3618a349c593b7edda7735
DAHYUN <88223753+sondahyun@users.noreply.github.com>
2025-07-21 22:59:19 +0900

    [level 0] Title: 세균 증식, Time: 0.05 ms, Memory: 75.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/120910. 세균 증식/세균 증식.java
@@ -0,0 +1,8 @@
+import java.lang.Math;
+class Solution {
+    public int solution(int n, int t) {
+        int answer = 0;
+        Math.pow(2, t);
+        return (int)Math.pow(2, t) * n;
+    }
+}
