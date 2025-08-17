d58961c816563f20ea96e60f0531ca1369b5f944
DAHYUN <88223753+sondahyun@users.noreply.github.com>
2025-08-17 18:03:09 +0900

    8/17

--- SSAFY/2/n2.py
+++ SSAFY/2/n2.py
@@ -2,9 +2,9 @@ import sys
 sys.stdin = open("input.txt", "r")
 sys.stdout = open("output.txt", "w")
 
-
-def circular_distance(a, b, m):
-    return (b - a + m)
+# a에서 b로 이동 (크기는 m)
+def distance(a, b, m):
+    return (b - a + m)%m
 
 
 t = int(input())
@@ -18,16 +18,18 @@ for test_case in range(1, t + 1):
 
     # 전체 거리 및 구간 거리 계산
     for i in range(n - 1):
-        d = circular_distance(n_list[i], n_list[i + 1], m)
-        dist.append(d)
+        d = distance(n_list[i], n_list[i + 1], m)
+        dist.append(d) 
         total_dis += d
 
     # 한 칸 제거 후 최소 거리 탐색
     min_dis = total_dis
     for i in range(1, n - 1):
         removed = dist[i - 1] + dist[i]
-        added = circular_distance(n_list[i - 1], n_list[i + 1], m)
+        added = distance(n_list[i - 1], n_list[i + 1], m)
         new_total = total_dis - removed + added
         min_dis = min(min_dis, new_total)
 
     print("#%d %d" % (test_case, min_dis))
+
+
