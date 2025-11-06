95123f0cc31950612e760aee06c97fe031b5cfdc
DAHYUN <88223753+sondahyun@users.noreply.github.com>
2025-11-06 23:31:50 +0900

    [Bronze V] Title: ??!, Time: 112 ms, Memory: 14308 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.java
@@ -0,0 +1,12 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static StringBuilder sb = new StringBuilder();
+    public static void main(String[] args) throws Exception {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        String s = in.readLine();
+
+        System.out.println(s + "??!");
+    }
+}
