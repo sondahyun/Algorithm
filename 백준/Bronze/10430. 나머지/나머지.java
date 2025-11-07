e35dbe760022a498e58f5a3cdbec13f261def68c
DAHYUN <88223753+sondahyun@users.noreply.github.com>
2025-11-07 22:02:44 +0900

    [Bronze V] Title: 나머지, Time: 104 ms, Memory: 14432 KB -BaekjoonHub

---
+++ 백준/Bronze/10430. 나머지/나머지.java
@@ -0,0 +1,17 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        String[] split = in.readLine().split(" ");
+        int A = Integer.parseInt(split[0]);
+        int B = Integer.parseInt(split[1]);
+        int C = Integer.parseInt(split[2]);
+
+        System.out.println((A+B)%C);
+        System.out.println(((A%C) + (B%C))%C);
+        System.out.println((A*B)%C);
+        System.out.println(((A%C)*(B%C))%C);
+    }
+}
