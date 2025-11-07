0c22053be5affd666e02a1558507fbfacd6fae23
DAHYUN <88223753+sondahyun@users.noreply.github.com>
2025-11-08 00:13:25 +0900

    [Bronze IV] Title: 별 찍기 - 2, Time: 216 ms, Memory: 18064 KB -BaekjoonHub

---
+++ 백준/Bronze/2439. 별 찍기 － 2/별 찍기 － 2.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(in.readLine());
+
+        for (int i = 1; i <= T; i++) {
+            for (int j = 0; j < T - i; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 0; j < i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+
+        }
+    }
+}
