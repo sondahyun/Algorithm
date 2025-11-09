a7899ebea2c691363e22b6268506c25063e0f0ea
DAHYUN <88223753+sondahyun@users.noreply.github.com>
2025-11-09 13:17:44 +0900

    [Bronze I] Title: 평균, Time: 116 ms, Memory: 16412 KB -BaekjoonHub

---
+++ 백준/Bronze/1546. 평균/평균.java
@@ -0,0 +1,32 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static StringBuilder sb = new StringBuilder();
+    public static void main(String[] args) throws Exception {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(in.readLine());
+        double[] array = new double[n];
+        String[] split = in.readLine().split(" ");
+        int max = Integer.MIN_VALUE;
+        int index = -1;
+        Double sum = 0.0;
+        for (int i = 0; i < n; i++) {
+            int num = Integer.parseInt(split[i]);
+            if (num > max) {
+                max = num;
+                index = i;
+            }
+        }
+        for (int i = 0; i < n; i++) {
+            array[i] = Double.parseDouble(split[i])/max*100;
+        }
+        for (int i = 0; i < n; i++) {
+            sum += array[i];
+        }
+        System.out.println(sum/n);
+
+
+    }
+}
