67863f62f78f6dc5eab0c99b8765152260f796e6
DAHYUN <88223753+sondahyun@users.noreply.github.com>
2025-11-10 17:15:55 +0900

    [Bronze V] Title: 킹, 퀸, 룩, 비숍, 나이트, 폰, Time: 116 ms, Memory: 15888 KB -BaekjoonHub

---
+++ 백준/Bronze/3003. 킹， 퀸， 룩， 비숍， 나이트， 폰/킹， 퀸， 룩， 비숍， 나이트， 폰.java
@@ -0,0 +1,17 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static StringBuilder sb = new StringBuilder();
+    public static void main(String[] args) throws Exception {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+
+        String[] split = in.readLine().split(" ");
+        int[] array = {1, 1, 2, 2, 2, 8};
+
+        for (int i = 0; i < 6; i++) {
+            System.out.print(array[i] - Integer.parseInt(split[i]) + " ");
+        }
+
+    }
+}
